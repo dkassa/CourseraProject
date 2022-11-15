2aa4c6ba5816f675981dd9f8c2085f03ff6b3d01
dkassa <desalegnashagrie@gmail.com>
2022-11-15 15:00:44 -0600

    Update config.js
    updated facebook

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '829375987467063',
-        clientSecret: '0a14cd483f5c5b6469a295a11e4e0cc9'
+        clientId: '664506011840764',
+        clientSecret: 'a156f68f1eb7c12af0bc84650449e1dc'
     }
 }
